867523f2825b4e1451d6b20a92cde602c39b7fc2
Nayeon Amy Kim <nykim0112@gmail.com>
2019-09-17 20:33:08 -0400

    Challenge completed!

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
 
+@app.route('/pokemon/<query>')
+def pokemon_info(query):
+    request_url="https://pokeapi.co/api/v2/pokemon/"+str(query)+"/"
+    response = requests.get(request_url)
+
+    # for debugging purposes
+    # print(response.status_code)
+
+    decoded_response = json.loads(response.text)
+
+    if ( query.isdigit() ):
+        return "The pokemon with id " + str(query) + " is " + decoded_response['name']
+
+    elif ( type(query) == str ):
+        return str(query) + " has id " + str(decoded_response['id'])
+
+    else:
+        return null
 
 if __name__ == '__main__':
     app.run()
